c689580998b096f4b46bff0ca5aa41663df30734
Arian H <arianhosseini@outlook.com>
2024-01-26 00:50:56 +0330

    feat: implemented the deployment script for the faucet contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "";
 
 const config: HardhatUserConfig = {
 	solidity: "0.8.23",
+	networks: {
+		sepolia: {
+			url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+			accounts: [process.env.PRIVATE_KEY || ""],
+		},
+	},
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,15 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+	console.log("Preparing deployment...\n");
 
-  const lockedAmount = ethers.parseEther("0.001");
+	const spectreFaucet = await ethers.deployContract("SpectreFaucet", ["0x4aFd39239Dca334F02e9F829700733B4DE048595"]);
+	await spectreFaucet.waitForDeployment();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+	console.log(`Spectre Faucet contract deployed to: ${await spectreFaucet.getAddress()}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
